56f56c2effde617fba5067d7878ea68fdd167f49
Bento Netto <bentoeemanuele@gmail.com>
2024-06-15 20:45:11 -0300

    Ajuste final de implementação

--- src/servidor.js
+++ src/servidor.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const servidor = express()
 
-servidor.get('/', function controlador (req, res){
+servidor.get('/home', function controlador (req, res){
     return resposta.send('Meu primeiro servidor Node.js com Express')
 })
 
